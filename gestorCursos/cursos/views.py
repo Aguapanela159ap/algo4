ca20fe7fbee2924e808314854d648f335c8df7e8
Aguapanela <79724498+Aguapanela159ap@users.noreply.github.com>
2024-12-05 02:47:35 -0500

    Add files via upload

---
+++ gestorCursos/cursos/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse, JsonResponse
+from cursos.models import Curso
+
+
+def crear_curso(request):
+                    if request.method == "POST":
+                        codigo = request.POST.get('codigo')
+                        nombre = request.POST.get('nombre')
+
+                        # Crear y guardar el curso en la base de datos
+                        curso = Curso(codigo=codigo, nombre=nombre)
+                        curso.save()
+
+                        # Retorna un mensaje de éxito o un estado 201 (creado)
+                        return JsonResponse({"message": "Curso creado exitosamente"}, status=201)
+
+                    return JsonResponse({"error": "Método no permitido"}, status=405)
+            
+    
+def lista_cursos(request):
+    cursos = Curso.objects.all()  # Obtener todos los cursos de la base de datos
+    cursos_list = [{"id": curso.id, "codigo": curso.codigo, "nombre": curso.nombre} for curso in cursos]
+    return JsonResponse(cursos_list, safe=False) 
